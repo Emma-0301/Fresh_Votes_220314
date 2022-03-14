a701398bdaabf28e1a059e1d840ee8c0b486407f
Emma_Lyy <845196029@qq.com>
2022-03-14 17:00:56 +0800

    User

---
+++ src/main/java/com/freshvotes/domain/User.java
@@ -0,0 +1,17 @@
+package com.freshvotes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Emma_Lyy
+ * @create 2022-03-14 15:50
+ */
+
+@Entity
+@Table(name="users")
+public class User {
+    private Integer id;
+
+
+}
